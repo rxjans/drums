41ab4fc58804eea77cb4c330f14aa956c51ac55f
rxjans <120579803+rxjans@users.noreply.github.com>
2023-07-23 20:00:15 +0530

    Update rj.js

--- rj.js
+++ rj.js
@@ -27,37 +27,37 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/0.mp3");
+      var tom1 = new Audio("./sounds/0.mp3");
       tom1.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/1.mp3");
+      var tom2 = new Audio("./sounds/1.mp3");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio('sounds/2.mp3');
+      var tom3 = new Audio('./sounds/2.mp3');
       tom3.play();
       break;
 
     case "d":
-      var tom4 = new Audio('sounds/3.mp3');
+      var tom4 = new Audio('./sounds/3.mp3');
       tom4.play();
       break;
 
     case "j":
-      var snare = new Audio('sounds/4.mp3');
+      var snare = new Audio('./sounds/4.mp3');
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio('sounds/5.mp3');
+      var crash = new Audio('./sounds/5.mp3');
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio('sounds/6.mp3');
+      var kick = new Audio('./sounds/6.mp3');
       kick.play();
       break;
 
